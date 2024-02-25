7218fbafad2d3a0ce1cc73cdd889a23c6649fc3e
0xhatsume <0xhatsume@gmail.com>
2024-02-24 21:30:43 -0700

    :sparkles: Add backlink button for create resource navbar

--- client/src/components/Navbar/navprojectintegration.tsx
+++ client/src/components/Navbar/navprojectintegration.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 
 export const NavProjectIntegration = () => {
+    const navigate = useNavigate()
+
     return (
         <div className="flex justify-start items-center
         py-1 mx-2
         ">
             <span className="bg-gray-500/20 rounded-full p-2 mx-4
             hover:cursor-pointer hover:bg-gray-700/50
-            ">
+            "
+            onClick={()=> navigate(-1)}
+            >
                 <FaArrowLeft />
             </span>
             <span className="text-xl">
