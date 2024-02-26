d78dba676580e911bfcb46458ecf396fb9902756
0xhatsume <0xhatsume@gmail.com>
2024-02-26 05:51:37 -0700

    :sparkles: Add wallet providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Provider as JotaiProvider } from 'jotai';
+import Web3Store from '@/stores/web3Store/Web3Store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <JotaiProvider>
+      <App />
+      <Web3Store />
+    </JotaiProvider>
   </React.StrictMode>,
 )
